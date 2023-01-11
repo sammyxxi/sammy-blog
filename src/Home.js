5cd5d71763456bb4801cc430f81fa35f48eabaff
Sammy Kavanagh <samkav025@gmail.com>
2023-01-11 16:53:19 +0900

    added error message

--- src/Home.js
+++ src/Home.js
@@ -4,24 +4,34 @@ import BlogList from './BlogList';
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
       .then(res => {
+        if(!res.ok) {
+          throw Error('Could not fetch the content for that resource');
+        }
         return res.json();
       })
       .then(data => {
         setBlogs(data);
         setIsPending(false);
-      });
+        setError(null);
+      })
+      .catch(err => {
+        setIsPending(false);
+        setError(err.message);
+      })
     }, 1000);
   }, []);
 
   return (
     <div className="home">
+      { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
-      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+      { blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
     </div>
   );
 }
