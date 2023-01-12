ab839350bb040d6e093118a81afc6a07d9e21a43
Sammy Kavanagh <samkav025@gmail.com>
2023-01-12 16:43:11 +0900

    added cleanup component

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,10 +6,12 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
       .then(res => {
-        if (!res.ok) { // error coming back from server
+        if (!res.ok) {
           throw Error('could not fetch the data for that resource');
         }
         return res.json();
@@ -20,11 +22,16 @@ const useFetch = (url) => {
         setError(null);
       })
       .catch(err => {
-        // auto catches network / connection error
-        setIsPending(false);
-        setError(err.message);
+        if (err.name === 'AbortError') {
+          console.log('fetch aborted')
+        } else {
+          setIsPending(false);
+          setError(err.message);
+        }
       })
     }, 1000);
+
+    return () => abortCont.abort();
   }, [url])
 
   return { data, isPending, error };
